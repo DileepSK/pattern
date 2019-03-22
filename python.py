fdf95436889a8d374974c2c3f5a5b875ab5929ee
DileepSK <47359772+DileepSK@users.noreply.github.com>
2019-03-22 10:38:02 +0530

    pattern file created

---
+++ python.py
@@ -0,0 +1,11 @@
+pattern printing program
+def pattern(n)
+   for i in range(50):
+	 print(' ' *(50-i),end =" ")
+	 print('*'*(i+1),end="")
+	 print('*'*i)
+
+n=int(input("enter the number")
+pattern(n)
+
+       
